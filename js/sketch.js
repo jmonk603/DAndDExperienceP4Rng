68b981f5cab622186dfc9411f9599f0c877f87f7
jmonk603 <91668513+jmonk603@users.noreply.github.com>
2025-05-15 15:11:02 +0100

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -131,11 +131,11 @@ function touchEnded(){
 
 function openLink(){
   if (storyCounter == 5){
-    window.open("https://jmonk603.github.io/DandDARWinDragRng/");
+    window.open("https://jmonk603.github.io/DAndDARWinDragRng/");
     window.location.href = ('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
   }
   if (storyCounter == 8){
-    window.open("https://jmonk603.github.io/DandDARLoseDragRng/");
+    window.open("https://jmonk603.github.io/DAndDARLoseDragRng/");
     window.location.href = ('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
   }
 }
